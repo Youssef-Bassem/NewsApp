75af40a9d86618cedcdb86c7551afe831b7f32db
PhilopateerWagdy <philowagdy20@gmail.com>
2021-08-22 14:47:27 +0200

    Splach Screen & Home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/Home.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: Home(),
     );
   }
 }
